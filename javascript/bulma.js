c5613a6b1fc1e3916118a5d24665b5a6d415a96b
michael-andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2019-06-10 09:42:41 +0300

    Update bulma.js

--- javascript/bulma.js
+++ javascript/bulma.js
@@ -7,3 +7,21 @@
                 menu.classList.toggle('is-active');
             });
         })();
+
+        // Drop Down
+
+        $(".navbar-item.has-dropdown").click(function(e) {
+            if ($(".navbar-burger").is(':visible')) {
+                $(this).toggleClass("is-active");
+            }
+        });
+        $(".navbar-item > .navbar-link").click(function(e) {
+            if ($(".navbar-burger").is(':visible')) {
+                e.preventDefault();
+            }
+        });
+        $(window).resize(function(e) {
+            if (!$(".navbar-burger").is(':visible') && $(".navbar-item.has-dropdown.is-active").length) {
+                $(".navbar-item.has-dropdown.is-active").removeClass('is-active');
+            }
+        });
